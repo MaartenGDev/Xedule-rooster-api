d46dc52b911f1a0f96260c76a146037d6cb0c930
MaartenGDev <maarten.grootoonk@gmail.com>
2016-09-12 19:38:49 +0200

    Added login

--- app/Client.php
+++ app/Client.php
@@ -2,6 +2,7 @@
 namespace App;
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Cookie\CookieJar;
 use MaartenGDev\CacheInterface;
 
 class Client
@@ -9,19 +10,16 @@ class Client
     private $client;
     private $parser;
 
-    private $userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36';
-    private $baseUrl = 'https://roosters.xedule.nl/Attendee/ChangeWeek/';
+    protected $userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36';
+    protected $baseUrl = 'https://roosters.xedule.nl/Attendee/ChangeWeek/';
 
-    private $group = '';
-    private $ordeId = 130;
-    private $code = 52257;
-    private $attId = 1;
+    protected $group = '';
+    protected $ordeId = 130;
+    protected $code = 52257;
+    protected $attId = 1;
 
-    private $result;
-    /**
-     * @var CacheInterface
-     */
-    private $cache;
+    protected $result;
+    protected $cache;
 
     public function __construct(ClientInterface $client, XeduleParser $parser, CacheInterface $cache)
     {
@@ -32,24 +30,56 @@ class Client
         $this->cache = $cache;
     }
 
+    /**
+     * Set the group.
+     *
+     * @param string $group The group name.
+     *
+     * @return void
+     */
     public function setGroup($group)
     {
         $this->group = $group;
     }
 
-    private function getApi()
+    /**
+     * Build the website
+     * url using the parameters.
+     *
+     * @return string
+     */
+    protected function getApi()
     {
-        return $this->baseUrl . $this->code . '?Code=' . $this->group . '&OreId=' . $this->ordeId . '&AttId=' . $this->attId;
+        $query = http_build_query(
+            [
+                'Code' => $this->group,
+                'OreId' => $this->ordeId,
+                'AttId' => $this->attId
+            ]
+        );
+
+        return $this->baseUrl . $this->code . '?' . $query;
     }
 
 
-    private function createForm($data,$week)
+    /**
+     * Checks if there is already a cache entry
+     * and gets the data if isn't a cache entry.
+     *
+     * @param array $data The form data
+     * @param int $week The week number
+     * @return $this
+     */
+    protected function createForm(array $data, $week)
     {
+
         $key = 'rooster' . $week . $this->group;
 
-        $cache = $this->cache->has($key, function ($cache) use($week,$key){
+        $cache = $this->cache->has(
+            $key, function ($cache) use ($week, $key) {
             return $this->result = $cache->get($key);
-        });
+        }
+        );
 
         if ($cache) {
             return $this;
@@ -61,28 +91,47 @@ class Client
             [
                 'form_params' => $data,
                 'headers' => [
-                    'User-Agent' => $this->userAgent
+                    'User-Agent' => $this->userAgent,
+                    'Cookie' => 'ASP.NET_SessionId=secureForSure; _ga=GA1.234.1234.123; _gat=1',
                 ]
             ]
         )->getBody();
 
-        $this->cache->store($key,$data);
+        $this->cache->store($key, $data);
 
         $this->result = $data;
         return $this;
     }
 
-    private function post()
+    /**
+     * Parse the html page returned using the parser.
+     *
+     * @return array
+     */
+    protected function post()
     {
         return $this->parser->parse($this->result)->allWeek();
     }
 
+    /**
+     * Get the week by sending a post request.
+     *
+     * @param integer $week The week number
+     * @param string $year The year of the week.
+     *
+     * @return array
+     */
     public function getWeek($week, $year = null)
     {
-        if ($year === null) $year = date('Y');
+        if ($year === null) {
+            $year = date('Y');
+        }
 
-        return $this->createForm([
-            'currentWeek' => "{$year}/{$week}"
-        ],$week)->post();
+        return $this->createForm(
+            [
+                'currentWeek' => "{$year}/{$week}"
+            ], $week
+        )
+            ->post();
     }
 }
